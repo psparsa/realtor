7e7066f440db1d7d97d7e0603b7d50f9fbfe4f2d
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2024-01-08 11:43:18 +0330

    implement signup logic

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { SignUpDTO } from './auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async signup({ email }: SignUpDTO) {
+  async signup({ email, password, name, phone }: SignUpDTO) {
     const userExists = !!(await this.prismaService.user.findUnique({
       where: {
         email,
@@ -14,5 +16,20 @@ export class AuthService {
     }));
 
     if (userExists) throw new ConflictException();
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const createdUser = await this.prismaService.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        phone,
+        userType: UserType.BUYER,
+      },
+    });
+
+    return createdUser;
   }
 }
