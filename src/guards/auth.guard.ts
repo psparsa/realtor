6c9eb34b62150b7e1786600beef6040f65a9d562
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2024-01-13 16:05:07 +0330

    add auth guard to protect urls

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,48 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import _ from 'lodash/fp';
+import * as jwt from 'jsonwebtoken';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface JWTPayload {
+  name: string;
+  id: number;
+}
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflactor: Reflector,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const roles = this.reflactor.getAllAndOverride('roles', [
+      context.getHandler(),
+    ]);
+    if (_.isEmpty(roles)) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const token = request?.headers?.authorization?.split(' ')?.[1];
+
+    try {
+      const payload = jwt.verify(
+        token,
+        process.env.JSON_TOKEN_KEY,
+      ) as JWTPayload;
+
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          id: payload.id,
+        },
+      });
+
+      if (!user) return false;
+      return roles.includes(user.userType);
+
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+}
--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -9,10 +9,14 @@ import {
   Put,
   Query,
   UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { CreateHomeDTO, UpdateHomeDTO } from './home.dto';
 import { User, UserInfo } from 'src/user/user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { Roles } from 'src/roles.decorator';
+import { UserType } from '@prisma/client';
 
 @Controller('home')
 export class HomeController {
@@ -39,11 +43,15 @@ export class HomeController {
     return this.homeService.getHome(id);
   }
 
+  @Roles([UserType.REALTOR])
+  @UseGuards(AuthGuard)
   @Post()
   createHome(@Body() body: CreateHomeDTO, @User() user: UserInfo) {
     return this.homeService.createHome(body, user.id);
   }
 
+  @Roles([UserType.REALTOR])
+  @UseGuards(AuthGuard)
   @Put(':id')
   async updateHome(
     @Param('id', ParseIntPipe) id: number,
@@ -55,6 +63,8 @@ export class HomeController {
     return this.homeService.updateHomeById(body, id);
   }
 
+  @Roles([UserType.REALTOR, UserType.ADMIN])
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deleteHome(
     @Param('id', ParseIntPipe) id: number,
